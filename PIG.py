08328cb198c4baef0ea7efa2da86f7a86d42c4bb
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-12 10:36:27 -0400

    Raised max score and added play instructions

--- PIG.py
+++ PIG.py
@@ -20,7 +20,7 @@ while True:
     else:
         print("Invalid, try again.")
 
-max_score = 50
+max_score = 100
 player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < max_score:
